d1cc5278546af4d7ded0bc0f2d295bb7bd3a88b9
Aleksa <aleksa.jovanovic232@gmail.com>
2025-01-27 12:09:53 +0100

    Test

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'API is working!']);
-});
+Route::post('/register', [AuthController::class, 'register']);
